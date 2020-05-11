e1fc482745ce0d1d3724c79176113678e738673f
Brian Dukes <bdukes@engagesoftware.com>
2020-05-11 09:33:53 -0500

    Format with Prettier

--- index.js
+++ index.js
@@ -1,32 +1,39 @@
 /*eslint-env node*/
 
-const _ = require('lodash');
+const _ = require("lodash");
 
 const ERROR_SEVERITY = 2;
 
 function logIssue(issue) {
     const attributes = _.chain(_.toPairs(issue))
-                        .map((pair) => ({ key: pair[0], value: pair[1], }))
-                        .filter((pair) => pair.key !== 'message')
-                        .filter((pair) => pair.value !== undefined)
-                        .map((pair) => `${pair.key}=${pair.value};`)
-                        .value();
+        .map(pair => ({ key: pair[0], value: pair[1] }))
+        .filter(pair => pair.key !== "message")
+        .filter(pair => pair.value !== undefined)
+        .map(pair => `${pair.key}=${pair.value};`)
+        .value();
 
-    return `##vso[task.logissue ${attributes.join('')}]${issue.message}`;
+    return `##vso[task.logissue ${attributes.join("")}]${issue.message}`;
 }
 
-module.exports = (results) =>
+module.exports = results =>
     _.chain(results)
-     .filter((result) => result.messages.length > 0)
-     .flatMap((result) => _.chain(result.messages)
-                           .map((message) => logIssue({
-                               type: (message.fatal || message.severity === ERROR_SEVERITY) ? 'error' : 'warning',
-                               sourcepath: result.filePath,
-                               linenumber: message.line,
-                               columnnumber: message.column,
-                               code: message.ruleId,
-                               message: message.message,
-                            }))
-                            .value())
-     .value()
-     .join('\n');
+        .filter(result => result.messages.length > 0)
+        .flatMap(result =>
+            _.chain(result.messages)
+                .map(message =>
+                    logIssue({
+                        type:
+                            message.fatal || message.severity === ERROR_SEVERITY
+                                ? "error"
+                                : "warning",
+                        sourcepath: result.filePath,
+                        linenumber: message.line,
+                        columnnumber: message.column,
+                        code: message.ruleId,
+                        message: message.message
+                    })
+                )
+                .value()
+        )
+        .value()
+        .join("\n");
