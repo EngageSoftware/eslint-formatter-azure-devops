8960b08dc3243685f3b28b1151bab75996028f5d
Wojciech Sendera <wsendera@neworbit.co.uk>
2022-11-15 12:51:51 +0100

    remove lodash

--- index.js
+++ index.js
@@ -1,50 +1,47 @@
 /*eslint-env node*/
 
-const _ = require("lodash");
-
 const ERROR_SEVERITY = 2;
 
 function logIssue(issue) {
-    const attributes = _.chain(_.toPairs(issue))
-        .map(pair => ({ key: pair[0], value: pair[1] }))
-        .filter(pair => pair.key !== "message")
-        .filter(pair => pair.value !== undefined)
-        .map(pair => `${pair.key}=${pair.value};`)
-        .value();
+    const attributes = Object.entries(issue)
+        .filter((pair) => pair[0] !== "message" && pair[1] !== undefined)
+        .map((pair) => `${pair[0]}=${pair[1]};`);
 
     return `##vso[task.logissue ${attributes.join("")}]${issue.message}`;
 }
 
 module.exports = (results, resultsMeta) => {
-    let shouldPrintMaxWarningsExceeded = false;
-    let maxWarningsExceededMessage;
-    if (resultsMeta?.maxWarningsExceeded != null && resultsMeta.maxWarningsExceeded.maxWarnings <= resultsMeta.maxWarningsExceeded.foundWarnings) {
-        shouldPrintMaxWarningsExceeded = true;
-        maxWarningsExceededMessage = logIssue({
-            type: "error",
-            // eslint-disable-next-line max-len
-            message: `ESLint found too many warnings (maximum: ${resultsMeta.maxWarningsExceeded.maxWarnings}, found: ${resultsMeta.maxWarningsExceeded.foundWarnings}).`,
-        });
-    }
-
-    const r = _.chain(results)
-        .filter(result => result.messages.length > 0)
-        .flatMap(result =>
-            _.chain(result.messages)
-                .map(message =>
-                    logIssue({
-                        type: message.fatal || message.severity === ERROR_SEVERITY ? "error" : "warning",
-                        sourcepath: result.filePath,
-                        linenumber: message.line,
-                        columnnumber: message.column,
-                        code: message.ruleId,
-                        message: message.message,
-                    })
-                )
-                .value()
+    const resultsAsString = results
+        .filter((result) => result.messages.length > 0)
+        .map((result) =>
+            result.messages.map((message) =>
+                logIssue({
+                    type:
+                        message.fatal || message.severity === ERROR_SEVERITY
+                            ? "error"
+                            : "warning",
+                    sourcepath: result.filePath,
+                    linenumber: message.line,
+                    columnnumber: message.column,
+                    code: message.ruleId,
+                    message: message.message,
+                })
+            )
         )
-        .value()
+        .reduce((previousValue, currentValue) => {
+            if (currentValue != null && currentValue.length > 0) {
+                return previousValue.concat(currentValue);
+            }
+            return previousValue;
+        }, [])
         .join("\n");
 
-    return shouldPrintMaxWarningsExceeded ? `${r}\n${maxWarningsExceededMessage}\n` : r;
+    return resultsMeta?.maxWarningsExceeded != null &&
+        resultsMeta.maxWarningsExceeded.maxWarnings <
+            resultsMeta.maxWarningsExceeded.foundWarnings
+        ? `${resultsAsString}\n${logIssue({
+              type: "error",
+              message: `ESLint found too many warnings (maximum: ${resultsMeta.maxWarningsExceeded.maxWarnings}, found: ${resultsMeta.maxWarningsExceeded.foundWarnings}).`,
+          })}\n`
+        : resultsAsString;
 };
--- test.js
+++ test.js
@@ -13,7 +13,7 @@ const fixture = [
                 line: 22,
                 column: 9,
                 nodeType: "Identifier",
-                source: "        initialNavHeight = $mainNavMenu.outerHeight();"
+                source: "        initialNavHeight = $mainNavMenu.outerHeight();",
             },
             {
                 ruleId: "consistent-return",
@@ -23,7 +23,7 @@ const fixture = [
                 line: 76,
                 column: 14,
                 nodeType: "FunctionDeclaration",
-                source: "    function hideSubmenu(menuItem) {"
+                source: "    function hideSubmenu(menuItem) {",
             },
             {
                 ruleId: "eqeqeq",
@@ -32,11 +32,11 @@ const fixture = [
                 line: 234,
                 column: 25,
                 nodeType: "BinaryExpression",
-                source: "        if (parentLevel == 1) {"
-            }
+                source: "        if (parentLevel == 1) {",
+            },
         ],
         errorCount: 0,
-        warningCount: 3
+        warningCount: 3,
     },
     {
         filePath: "C:\\js\\errors.js",
@@ -48,7 +48,7 @@ const fixture = [
                 line: 2,
                 column: 26,
                 nodeType: "Identifier",
-                source: "void function themeStart($) {"
+                source: "void function themeStart($) {",
             },
             {
                 ruleId: "no-constant-condition",
@@ -57,7 +57,7 @@ const fixture = [
                 line: 5,
                 column: 5,
                 nodeType: "IfStatement",
-                source: "    if (x = 10) {"
+                source: "    if (x = 10) {",
             },
             {
                 ruleId: "no-cond-assign",
@@ -67,7 +67,7 @@ const fixture = [
                 line: 5,
                 column: 9,
                 nodeType: "IfStatement",
-                source: "    if (x = 10) {"
+                source: "    if (x = 10) {",
             },
             {
                 ruleId: "no-undef",
@@ -76,54 +76,54 @@ const fixture = [
                 line: 5,
                 column: 9,
                 nodeType: "Identifier",
-                source: "    if (x = 10) {"
-            }
+                source: "    if (x = 10) {",
+            },
         ],
         errorCount: 4,
-        warningCount: 0
+        warningCount: 0,
     },
     {
         filePath: "C:\\js\\good.js",
         messages: [],
         errorCount: 0,
-        warningCount: 0
-    }
+        warningCount: 0,
+    },
 ];
 
-test("Given no results, logs nothing", assert => {
+test("Given no results, logs nothing", (assert) => {
     const results = [];
 
     assert.is(formatter(results), "");
 });
 
-test("Given results with no messages, logs nothing", assert => {
+test("Given results with no messages, logs nothing", (assert) => {
     const results = [
         {
             filePath: "tmp/good.js",
             messages: [],
             errorCount: 0,
-            warningCount: 0
+            warningCount: 0,
         },
         {
             filePath: "tmp/good2.js",
             messages: [],
             errorCount: 0,
-            warningCount: 0
-        }
+            warningCount: 0,
+        },
     ];
 
     assert.is(formatter(results), "");
 });
 
-test("Given results with errors, logs errors", assert => {
+test("Given results with errors, logs errors", (assert) => {
     assert.regex(formatter(fixture), /##vso\[task\.logissue type=error/);
 });
 
-test("Given results with warnings, logs warnings", assert => {
+test("Given results with warnings, logs warnings", (assert) => {
     assert.regex(formatter(fixture), /##vso\[task\.logissue type=warning/);
 });
 
-test("Given results with 7 issues, logs 7 issues in valid format", assert => {
+test("Given results with 7 issues, logs 7 issues in valid format", (assert) => {
     assert.regex(
         formatter(fixture),
         /^(?:##vso\[task\.logissue ((?:type|sourcepath|linenumber|columnnumber|code)=[^;]+;)+\][^\n]+(?:\n|$)){6,7}$/
@@ -139,5 +139,37 @@ test("Given results with 7 issues, logs 7 issues in valid format", assert => {
     assert.notRegex(
         formatter(fixture),
         /^(?:##vso\[task\.logissue ((?:type|sourcepath|linenumber|columnnumber|code)=[^;]+;)+\][^\n]+(?:\n|$)){8,9}$/
+    );
+});
+
+test("Given results meta, logs too many warnings error", (assert) => {
+    assert.regex(
+        formatter([], {
+            maxWarningsExceeded: {
+                maxWarnings: 7,
+                foundWarnings: 9,
+            },
+        }),
+        /##vso\[task.logissue type=error;\]ESLint found too many warnings \(maximum: 7, found: 9\)\./
+    );
+    const regex = /ESLint found too many warnings/gis;
+    assert.notRegex(formatter(fixture), regex);
+    assert.notRegex(
+        formatter([], {
+            maxWarningsExceeded: {
+                maxWarnings: 12,
+                foundWarnings: 9,
+            },
+        }),
+        regex
+    );
+    assert.notRegex(
+        formatter([], {
+            maxWarningsExceeded: {
+                maxWarnings: 9,
+                foundWarnings: 9,
+            },
+        }),
+        regex
     );
 });
